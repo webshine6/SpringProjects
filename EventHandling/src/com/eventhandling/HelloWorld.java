659f6ef1807371ccc2ebdbd56036f858c6f088b9
Ramona <webshine6@abv.bg>
2016-09-23 18:47:36 +0300

    Application Context events handling using ApplicationListener interface - ContextStartedEvent and ContextStopedEvent example

---
+++ EventHandling/src/com/eventhandling/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.eventhandling;
+
+public class HelloWorld {
+
+	private String message;
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public void getMessage() {
+		System.out.println("Your message: " + message);
+	}
+
+}
